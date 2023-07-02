9112534831ab7bf6da87e9ce11ee0f7a1414fc88
ArcticAngel <mhykolwave@gmail.com>
2023-07-02 13:25:07 -0300

    added ignore file
    ignores node_modules, json files, and itself

--- Bot.js
+++ Bot.js
@@ -1,10 +1,11 @@
 /*UnamedBot project
 Start Date: July 1st, 2023
-Authors: Arctic_Angel, Kizune*/
+Authors: Arctic_Angel, Kizune
+https://discord.com/api/oauth2/authorize?client_id=1124899458073759814&permissions=8&scope=bot%20applications.commands*/
 
-const {client, Events, GatewayIntentBits} = require("discord.js");// Client, Events, and GatewayIntentBits all are part of the discord.js lib
-const token = "MTEyNDg5OTQ1ODA3Mzc1OTgxNA.Ga8T0W.7fprlux2PlYMoAiIdIf5MSSZXrSJ7dtaYIK_oQ";
-const client = new Client({ intents: [GatewayIntentBits.Guilds]});//Makes the bot user with each intent. If we apply for more intents they can be added here
+const {Client, Events, GatewayIntentBits} = require("discord.js");// Client, Events, and GatewayIntentBits all are part of the discord.js lib
+const {token} = require("./config.json");
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });//Makes the bot user with each intent. If we apply for more intents they can be added here
 
 //onetime run once client is okay to run
 client.once(Events.ClientReady, c => {
