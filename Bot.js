9001b4ed0a705ba645bdc7053566e99dc381e295
ArcticAngel <mhykolwave@gmail.com>
2023-07-02 00:20:59 -0300

    Added Bot.js
    Main Bot file

---
+++ Bot.js
@@ -0,0 +1,3 @@
+/*this will be our main run file, feel free to do what you want. Rearrange, rename, write it whatever. You've been invited to the app team on discords dev site.
+You can edit the bot app in the team.
+7/2/23 - 12:18am | Arc */
