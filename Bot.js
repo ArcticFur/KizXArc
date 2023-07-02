903e40117f6355818fc199503ae6695356a3ee0f
YakoKizune <111616533+YakoKizune@users.noreply.github.com>
2023-07-01 21:43:14 -0700

    Update Bot.js

--- Bot.js
+++ Bot.js
@@ -1,3 +1,9 @@
 /*this will be our main run file, feel free to do what you want. Rearrange, rename, write it whatever. You've been invited to the app team on discords dev site.
 You can edit the bot app in the team.
 7/2/23 - 12:18am | Arc */
+
+/*  Damn! even got the time stamps!
+    7/1/23 - 9:39 PM | Kiz  */
+
+
+//time to learn javascript
