ed20446a5b784024705abe888d37e62651202884
YakoKizune <111616533+YakoKizune@users.noreply.github.com>
2023-07-07 22:30:37 -0500

    added separate events for scalability

--- Bot.js
+++ Bot.js
@@ -31,34 +31,19 @@ for (const file of fs.readdirSync(foldersPath)) {
 }
 // <-
 
-//Onetime run once client is okay to run
-client.once(Events.ClientReady, c => {
-    console.log(`Ready! Logged in as ${c.user.tag}`);
-});
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
 
-//On interaction with user
-client.on(Events.InteractionCreate, async interaction => {
-	if (!interaction.isChatInputCommand()) return;// Not a command, return
-
-	const command = interaction.client.commands.get(interaction.commandName); //Command is equal to the slash command the user tried to input
-
-	//Command does not exist
-	if (!command) {
-		console.error(`No command matching ${interaction.commandName} was found.`);
-		return;
+for (const file of eventFiles) {
+	const filePath = path.join(eventsPath, file);
+	const event = require(filePath);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
 	}
-	//Attempt to run command
-	try {
-		await command.execute(interaction);
-	} catch (error) {
-		console.error(error);
-		if (interaction.replied || interaction.deferred) {
-			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
-		} else {
-			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-		}
-	}
-});
+}
+
 
 client.login(token);//Login to the bot user (Make sure last so it runs everything beforehand)
 
---
+++ events/ready.js
@@ -0,0 +1,10 @@
+//Onetime run once client is okay to run
+const { Events } = require('discord.js');
+
+module.exports = {
+	name: Events.ClientReady,
+	once: true,
+	execute(client) {
+		console.log(`Ready! Logged in as ${client.user.tag}`);
+	},
+};
